2a2fbbfc7282814cdcaf7334607498cfc5fa01e2
mingwiki <mingwiki@gmail.com>
2025-03-29 13:29:04 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,19 +2,17 @@ module.exports = {
   apps: [
     {
       name: "API Hub",
-      script: "/root/.pyenv/versions/apihub/bin/uvicorn",
+      script: "uvicorn",
       args: "main:app --port 9999 --no-access-log --loop uvloop",
-      cwd: "/root/apihub",
-      interpreter: "/root/.pyenv/versions/apihub/bin/python",
+      interpreter: ".venv/bin/python",
       env: {
         ENV: "prod",
       },
     },
     {
       name: "Scheduler",
-      script: "/root/.pyenv/versions/apihub/bin/python",
-      args: "scheduler.py",
-      cwd: "/root/apihub",
+      script: "scheduler.py",
+      interpreter: ".venv/bin/python",
       env: {
         ENV: "prod",
       },
