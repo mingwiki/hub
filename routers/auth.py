d99657195c7cfe947682ce5c00335acbe32933cd
mingwiki <mingwiki@gmail.com>
2025-04-05 23:48:35 +0800

    move to database

--- routers/auth.py
+++ routers/auth.py
@@ -6,15 +6,12 @@ from jose import JWTError, jwt
 from passlib.context import CryptContext
 from pydantic import BaseModel
 
-from models import db
+from models import db, get_config
 from utils import logger
 
 router = APIRouter(tags=["Authorization"])
 log = logger(__name__)
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-SECRET_KEY = "helloworld"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
 
 
@@ -26,11 +23,12 @@ def get_password_hash(password: str):
     return pwd_context.hash(password)
 
 
-def create_access_token(data: dict):
+async def create_access_token(data: dict):
+    jwt_config = await get_config("jwt_config")
     to_encode = data.copy()
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now() + timedelta(minutes=jwt_config.expire_minutes)
     to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return jwt.encode(to_encode, jwt_config.secret_key, algorithm=jwt_config.algorithm)
 
 
 async def authenticate_user(username: str, password: str):
@@ -41,13 +39,14 @@ async def authenticate_user(username: str, password: str):
 
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
+    jwt_config = await get_config("jwt_config")
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="无法验证token",
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, jwt_config.secret_key, algorithms=[jwt_config.algorithm])
         username = payload.get("sub")
         if username is None:
             raise credentials_exception
