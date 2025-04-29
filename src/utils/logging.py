eac0dcbb102c72f7e57fdccd14c8db7a1eca40f4
mingwiki <mingwiki@gmail.com>
2025-04-29 12:14:13 +0800

    update

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -16,7 +16,7 @@ if env == "prod":
     from sentry_sdk.integrations.logging import LoggingIntegration
 
     sentry_sdk.init(
-        "https://311f74ccaf5b994e142af119f09b6267@o4508725934948352.ingest.us.sentry.io/4508725939273728",
+        "https://33e4e07829584c17829c61a4284c67e5@bug.api.zed.ink/1",
         send_default_pii=True,
         max_request_body_size="always",
         traces_sample_rate=0,
