312be4afac1e63ed599e5aaf3d12cc0e5856c7fd
mingwiki <mingwiki@gmail.com>
2025-05-03 20:02:02 +0800

    add sentry dsn

--- routers/auth.py
+++ routers/auth.py
@@ -33,7 +33,9 @@ async def user_info(current_user: User = Depends(get_current_user)):
     return {
         "id": current_user.id,
         "username": current_user.username,
+        "email": current_user.email,
         "is_active": current_user.is_active,
+        "is_admin": current_user.is_admin,
     }
 
 
